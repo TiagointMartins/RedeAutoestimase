890b9308d34b690b36581c2bbeab0eae9320dd6f
TiagointMartins <tiago.sanmart@gmail.com>
2021-05-31 21:41:04 -0300

    Adicionando Repository: Usuario, Categoria e Postagem

---
+++ Blog/src/main/java/com/redeautoestimase/Blog/Repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.redeautoestimase.Blog.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.redeautoestimase.Blog.Model.Categoria;
+
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
---
+++ Blog/src/main/java/com/redeautoestimase/Blog/Repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.redeautoestimase.Blog.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.redeautoestimase.Blog.Model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+}
